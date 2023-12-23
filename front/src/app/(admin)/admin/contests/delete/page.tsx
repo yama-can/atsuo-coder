3e65919158edd14a9e603b0e30f028effb204d22
Yama.can <yama.can95@gmail.com>
2023-12-23 10:42:23 +0900

    [add] Add edit & delete contest gui

---
+++ front/src/app/(admin)/admin/contests/delete/page.tsx
@@ -0,0 +1,51 @@
+import { getContest } from "@/app/(pages)/contests/[contest]/contests";
+import styles from "../form.module.css";
+import { sql } from "@/app/sql";
+import { notFound } from "next/navigation";
+
+export default async function AdminContestsNew({ searchParams }: { searchParams: { id?: string } }) {
+
+	if (searchParams.id) {
+
+		const contests = await getContest(sql, searchParams.id);
+
+		if (contests.length == 0) {
+			notFound();
+		}
+		
+		return (
+			<>
+				<h1>Delete Contest | AtsuoCoder Admin</h1>
+				<p>Deleting {searchParams.id}</p>
+				<br />
+				<div className={styles.body1}>
+					<form action="/admin/contests/post" method="post">
+						<input type="hidden" name="type" defaultValue="delete" />
+						<p>Are you sure to delete this contest <code>{searchParams.id}</code>?</p>
+						<br />
+						<input name="check" id="check" type="checkbox" required className={styles.check} />
+						<label htmlFor="check">Yes, I'm sure.</label>
+						<br />
+						<input type="submit" defaultValue="Delete" className={styles.delete} />
+					</form>
+				</div>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h1>Delete Contest | AtsuoCoder Admin</h1>
+			<div className={styles.body1}>
+				<form action="/admin/contests/delete" method="get">
+					<label htmlFor="id">ID</label>
+					<br />
+					<input name="id" id="id" type="text" autoComplete="on" placeholder="aac001" required />
+					<br />
+					<input type="submit" defaultValue="Delete" className={styles.delete} />
+				</form>
+			</div>
+		</>
+	)
+
+}
---
+++ front/src/app/(admin)/admin/contests/edit/page.tsx
@@ -0,0 +1,101 @@
+import { getContest } from "@/app/(pages)/contests/[contest]/contests";
+import styles from "../form.module.css";
+import { sql } from "@/app/sql";
+import { notFound } from "next/navigation";
+
+export default async function AdminContestsNew({ searchParams }: { searchParams: { id?: string } }) {
+
+	if (searchParams.id) {
+
+		const contests = await getContest(sql, searchParams.id);
+
+		if (contests.length == 0) {
+			notFound();
+		}
+
+		const contest = contests[0];
+
+		return (
+			<>
+				<h1>Edit Contest | AtsuoCoder Admin</h1>
+				<p>Editing {searchParams.id}</p>
+				<br />
+				<div className={styles.body1}>
+					<form action="/admin/contests/post" method="post">
+						<input type="hidden" name="type" defaultValue="new" />
+						<label htmlFor="name">Name</label>
+						<br />
+						<input name="name" id="name" type="text" autoComplete="on" placeholder="AtsuoCoder Algorithm Contest 001" required defaultValue={contest.name} />
+						<br />
+						<label htmlFor="id">ID</label>
+						<br />
+						<input name="id" id="id" type="text" autoComplete="on" placeholder="aac001" required disabled defaultValue={searchParams.id} />
+						<br />
+						<label htmlFor="start">Start</label>
+						<br />
+						<input name="start" id="start" type="datetime-local" required defaultValue={new Date(contest.start).toISOString().slice(0, 16)} />
+						<br />
+						<label htmlFor="period">Period</label>
+						<br />
+						<input name="period" id="period" type="number" required className={styles.period} placeholder="100" defaultValue={contest.period} />
+						<label htmlFor="period">minutes</label>
+						<br />
+						<label htmlFor="penalty">Penalty</label>
+						<br />
+						<p>If this contest should be permanent contest, set this field "-1".</p>
+						<input name="penalty" id="penalty" type="number" required className={styles.period} placeholder="5" />
+						<label htmlFor="penalty">minutes</label>
+						<br />
+						<label htmlFor="problems">Problems</label>
+						<br />
+						<input name="problems" id="problems" type="text" required placeholder="aac001_a, aac001_b, aac001_c ... , aac001_f" defaultValue={contest.problems.join(', ')} />
+						<br />
+						<label htmlFor="editor" className={`${styles.warning} ${styles.show}`} id="editor-warning">
+							<ul>
+								<li> Warning: we will not warn if this field includes invalid problem ID.</li>
+							</ul>
+						</label>
+						<br />
+						<label htmlFor="editor">Editor</label>
+						<br />
+						<input name="editor" id="editor" type="text" required placeholder="yama_can, abn48" defaultValue={contest.editor.join(', ')} />
+						<br />
+						<label htmlFor="editor" className={`${styles.warning} ${styles.show}`} id="editor-warning">
+							<ul>
+								<li> Warning: we will not warn if this field includes invalid username.</li>
+							</ul>
+						</label>
+						<br />
+						<label htmlFor="tester">Tester</label>
+						<br />
+						<input name="tester" id="tester" type="text" required placeholder="yama_can, abn48" defaultValue={contest.tester.join(', ')} />
+						<br />
+						<label htmlFor="editor" className={`${styles.warning} ${styles.show}`} id="editor-warning">
+							<ul>
+								<li> Warning: we will not warn if this field includes invalid username.</li>
+							</ul>
+						</label>
+						<br />
+						<input type="submit" defaultValue="Create" />
+					</form>
+				</div>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h1>Edit Contest | AtsuoCoder Admin</h1>
+			<div className={styles.body1}>
+				<form action="/admin/contests/edit" method="get">
+					<label htmlFor="id">ID</label>
+					<br />
+					<input name="id" id="id" type="text" autoComplete="on" placeholder="aac001" required />
+					<br />
+					<input type="submit" defaultValue="Edit" />
+				</form>
+			</div>
+		</>
+	)
+
+}
